f58b1691f63ed285781ed66dd5af588318e4fa46
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-05-03 15:36:56 -0400

    inite

--- PuppyBell/PuppyBellApp.swift
+++ PuppyBell/PuppyBellApp.swift
@@ -1,17 +1,33 @@
-//
 //  PuppyBellApp.swift
 //  PuppyBell
 //
 //  Created by Lingyi Zhou on 5/3/25.
 //
-
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
 
 @main
 struct PuppyBellApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
         }
-    }
 }
