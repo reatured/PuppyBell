c4cdd46cbf974c7db3051d9f0c26d79446b61bf1
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-05-03 14:48:37 -0400

    Initial Commit

---
+++ PuppyBell/PuppyBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppyBellApp.swift
+//  PuppyBell
+//
+//  Created by Lingyi Zhou on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PuppyBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
